e68859375ab1641ec20984149d1215ca16ee134e
Aaron Whipple <ohhellwhipple@gmail.com>
2021-02-09 18:29:59 -0700

    finished piece minus key for secure

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'http://localhost:5001/clone-c91ab/us-central1/api'
+  baseURL: 'https://us-central1-clone-c91ab.cloudfunctions.net/api'
+  // local
+  // 'http://localhost:5001/clone-c91ab/us-central1/api'
 });
 
 export default instance;
